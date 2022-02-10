23e1a1f16ca06d187b4ead4567283b26f4464ec9
Matheus Bessa <mdabessa@gmail.com>
2022-02-10 05:14:43 -0300

    Create methods to Models

--- modules/models.py
+++ modules/models.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import  create_engine, Column, Integer, String, Float, ForeignKey
 from sqlalchemy.orm import sessionmaker, relationship
@@ -26,6 +27,15 @@ class Model(Base):
         if session:
             session.add(self)
             session.commit()
+
+
+    def delete(self):
+        self.super().delete()
+        session.commit()
+
+
+    def update(self):
+        session.commit()
 
 
     @classmethod
@@ -62,7 +72,7 @@ class Header(Model):
 
     def set(self, value):
         self.value = str(value)
-        session.commit()
+        self.update()
 
 
     @classmethod    
@@ -76,9 +86,51 @@ class Coin(Model):
     __tablename__ = 'coins'
 
     symbol = Column(String, unique=True, nullable=False)
-    historic = Column(ARRAY(Float))
+    historic = Column(ARRAY(Float), default=[])
     trades = relationship('Trade', back_populates='coin')
     wallet = relationship('Wallet', back_populates='coin')
+
+    def __init__(self, symbol: str, price: float, keep_historic: int, lot_size: str) -> None:
+        super().__init__(symbol=symbol)
+
+        self.keep_historic = keep_historic 
+        self.lot_size = lot_size
+
+        self.set_price(price)
+
+
+    def set_price(self, price: float) -> None:
+        self.price = price
+        self.addHistoric(price)
+        self.update()
+
+
+    def add_historic(self, price: float) -> None:
+        self.historic.append(price)
+        self.update()
+
+    
+    def quantity_lot_size(self, quantity: float) -> str:
+        if float(self.lot_size) == 1:
+            _qtd = int(quantity)
+        else:
+            q = Decimal(quantity-float(self.lot_size))
+            _qtd = q.quantize(Decimal(str(float(self.lot_size))))
+        
+        return str(_qtd)
+
+
+    def __repr__(self) -> str:
+        return f'<{self.symbol}: {self.price} USDT>'
+
+
+    def __bool__(self) -> bool:
+        return True
+
+
+    @classmethod
+    def get(cls, symbol: str):
+        return cls.select_one(cls.symbol == symbol)
 
 
 class Trade(Model):
@@ -94,9 +146,55 @@ class Wallet(Model):
     __tablename__ = 'wallet'
 
     quantity = Column(Float, nullable=False, default=0.0)
-    coin_id = Column(Integer, ForeignKey('coins.id'))
+    coin_id = Column(Integer, ForeignKey('coins.id'), unique=True, nullable=False)
     coin = relationship('Coin', back_populates='wallet')
 
+    def __init__(self, coin: Coin, quantity: float = 0.0, fee: float = 0.1) -> None:
+        super().__init__(coin, quantity)
+        
+        self.fee = fee
+
+
+    def set(self, quantity: float) -> None:
+        self.quantity = quantity
+        self.update()
+
+    
+    @classmethod
+    def buy(cls, coin: Coin, quantity: float = None) -> None:
+        usdt = cls.select_one(cls.coin.symbol == 'USDT')
+        if usdt.quantity <= 12:
+            raise Exception('Insufficient money!')
+        
+        if quantity == None:
+            quantity = usdt.quantity/coin.price
+        
+        wallet_coin = coin.wallet
+
+        wallet_coin.set(wallet_coin.quantity + (quantity * (1 - wallet_coin.fee/100)))
+        usdt.set(usdt.quantity - (quantity * coin.price))
+    
+
+    @classmethod
+    def sell(cls, coin: Coin, quantity: float = None) -> None:
+        usdt = cls.select_one(cls.coin.symbol == 'USDT')
+
+        wallet_coin = coin.wallet
+        if quantity == None:
+            quantity = wallet_coin.quantity
+
+        usdt.set(usdt.quantity + (quantity * coin.price) * (1 - usdt.fee/100))
+        wallet_coin.set(wallet_coin.quantity - quantity)
+
+
+    @classmethod
+    def to_dict(cls) -> dict:
+        coins = cls.select_all()
+        res = {}
+        for coin in coins:
+            res[coin.coin.symbol] = coin.quantity
+
+        return res 
 
 
 Model.metadata.create_all(engine)
