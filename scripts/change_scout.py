d97733c14a827ffd0f5ad7cce33329fcda4d1e2a
Matheus Bessa <mdabessa@gmail.com>
2022-04-02 04:02:05 -0300

    Create change_scout script

---
+++ scripts/change_scout.py
@@ -0,0 +1,12 @@
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(sys.path[0])))
+
+from modules.models import Header
+
+
+scout = Header.get('scout')
+print(f'Scout: {scout.evaluate()}')
+scout.set('0')
+print(f'Scout: {scout.evaluate()}')
